cd8dd5542df36f4001b9d245d42125aa7c9e376f
Victoire Lefebvre <guylaine.a@wanadoo.fr>
2020-05-04 16:24:09 +0200

    ex2 partie2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
-function recupText(){
-  var Text1 = document.getElementById (Text1).value;
-  alert (Text1);
+function recupText(){ //fonction de récupération du texte
+  var text1 = document.getElementById('text1').value;
+  // déclaration variable text1 et récupération la valeur de l'input text1
+  alert(text1); // boite de dialogue qui affiche la variable text1 ou la valeur input
 }
